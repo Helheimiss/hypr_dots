9b22ad59bb1c7209850fb058394b1a1b8c50da63
Helheimiss <171362259+Helheimiss@users.noreply.github.com>
2025-10-06 00:47:11 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+pacman -S --neended nwg-look ly ttf-firacode-nerd otf-font-awesome imv mpv ranger bat \
+    playerctl pavucontrol networkmanager htop thunar alacritty xdg-desktop-portal-hyprland \
+    hyprland pipewire waybar pipewire-pulse wireplumber wofi dunst \
+sustemctl enable ly
