34d39ffa92e67e93e5300d4d0b3d5ce654b4a933
Helheimiss <171362259+Helheimiss@users.noreply.github.com>
2025-10-06 21:42:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pacman -S --neended nwg-look ly ttf-firacode-nerd otf-font-awesome imv mpv ranger bat \
+pacman -S --needed nwg-look ly ttf-firacode-nerd otf-font-awesome gnu-free-fonts imv mpv ranger bat \
     playerctl pavucontrol networkmanager htop thunar alacritty xdg-desktop-portal-hyprland \
-    hyprland pipewire waybar pipewire-pulse wireplumber wofi dunst \
-sustemctl enable ly
+    hyprland pipewire waybar pipewire-pulse wireplumber wofi dunst
+
